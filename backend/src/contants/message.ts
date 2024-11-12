b5191a3b54cd0656fc346bb55795b6fcb6a13b29
vankha1 <vovankha2003@gmail.com>
2024-11-12 19:50:10 +0700

    chore: add crypto and message response

---
+++ backend/src/contants/message.ts
@@ -0,0 +1,18 @@
+export const authMess = {
+  LOGOUT: 'Logout ok!',
+  LOGIN: 'Login successful!',
+  NOT_CORRECT: 'Email or password not correct',
+  EMAIL_EXISTED: 'Email has adready existed',
+};
+
+export const userMess = {
+  UPDATED: 'Updated user successful',
+  FOUNED_ONE: 'Founded this user successful',
+  FOUNDED_MANY: 'Founded users successful',
+  DELETED: 'Deleted user successful',
+  NOT_FOUND: 'Not found this user',
+  NOT_CORRECT: 'Password or email not correct',
+  CHANGE_PASSWORD: 'Change password successful',
+  CREATED: 'Create user successful',
+  FORBIDDEN: 'Permission denined',
+};
