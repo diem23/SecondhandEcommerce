4a5ac7fc7de64aa551b844ae455e1677336b6b73
vankha1 <vovankha2003@gmail.com>
2024-11-12 19:48:38 +0700

    init project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './users';
+import { AuthModule } from './auth/auth.module';
+import { envConfig } from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [envConfig]
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (config: ConfigService) => {
+        return {
+          uri: config.get<string>('database'),
+        }
+      },
+      inject: [ConfigService],
+    }),
+
+    UserModule,
+    AuthModule
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService)
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Ecommerce services')
+    .setDescription('The ecommerce services API description')
+    .setVersion('0.1')
+    // .addBearerAuth()
+    .addTag('Ecommerce')
+    .build()
+
+  const documentFactory = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  await app.listen(config.get<number>('port'))
 }
 bootstrap();
