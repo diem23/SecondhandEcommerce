835f4f6ab70c3cba10742ca855176dec9e92a96e
vankha1 <vovankha2003@gmail.com>
2024-11-25 17:50:25 +0700

    feat: upload image with cloudinary

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,9 +1,11 @@
 export default () => ({
-    port: parseInt(process.env.PORT),
-    database: process.env.DATABASE_URI,
-    secret: process.env.SECRET,
-    expired: process.env.EXPIRED,
-    // client_host: process.env.CLIENT_HOST,
-    client_host_local: process.env.CLIENT_HOST_LOCAL,
-  })
-  
+  port: parseInt(process.env.PORT),
+  database: process.env.DATABASE_URI,
+  secret: process.env.SECRET,
+  expired: process.env.EXPIRED,
+  // client_host: process.env.CLIENT_HOST,
+  client_host_local: process.env.CLIENT_HOST_LOCAL,
+  cloud_name: process.env.CLOUD_NAME,
+  cloud_api_key: process.env.CLOUD_API_KEY,
+  cloud_api_secret: process.env.CLOUD_API_SECRET,
+});
